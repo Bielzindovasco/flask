7d990da65e09da942f937c77110d438c3a831a07
Bielzindovasco <124696240+Bielzindovasco@users.noreply.github.com>
2024-07-27 21:35:08 -0300

    Update app.py

--- Trabalho/app.py
+++ Trabalho/app.py
@@ -1,35 +1,35 @@
 from flask import *
-from bd import Playlist
+from bd import musica
 
 app = Flask(__name__)
 pl = Playlist
 
 @app.route('/')
-def listar_playlist():
-    playlists = pl.listar_playlist()
-    return render_template("playlist.html", playlists=playlists)
+def listar_musica():
+    musicas = pl.listar_musicas()
+    return render_template("musica.html", musicas=musicas)
 
 @app.route("/remover/<int:chave>")
 def apagar(chave):
-    pl.remove_playlist(chave)
+    pl.remove_musica(chave)
     return redirect('/')
 
-@app.route("/nova", methods=['GET', 'POST'])
+@app.route("/nova_musica", methods=['GET', 'POST'])
 def cadastro():
     if request.method == 'POST':
-        dados = request.form.to_dict()
-        pl.nova_playlist(dados.get('nome'), dados.get('tipo'), dados.get('artista'))
+        ds = request.form.to_dict()
+        pl.nova_musica(dados.get('nome'), ds.get('tipo'), dados.get('artista'))
         return redirect('/')
-    return render_template('playlist_form.html', playlist=None, title='Nova Playlist')
+    return render_template('musica_form.html', musica=None, title='Nova Musica')
 
 @app.route("/editar/<int:chave>", methods=['GET', 'POST'])
 def editar(chave):
     if request.method == 'POST':
-        dados = request.form.to_dict()
-        pl.atualiza_playlist(chave, dados.get('nome'), dados.get('tipo'), dados.get('artista'))
+        ds = request.form.to_dict()
+        pl.atualiza_musica(chave, ds.get('nome'), dados.get('tipo'), dados.get('artista'))
         return redirect('/')
-    playlist = pl.atualiza_playlist(chave)
-    return render_template('playlist_form.html', playlist=playlist, title='Editar Playlist')
+    musica = pl.atualiza_musica(chave)
+    return render_template('musica_form.html', musica=musica, title='Editar Musica')
 
 @app.route('/artista')
 def listar_artistas():
