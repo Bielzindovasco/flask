465c16b2109636b6dfa98746f5a2cc8df6b8a0f0
Bielzindovasco <124696240+Bielzindovasco@users.noreply.github.com>
2024-07-27 22:31:12 -0300

    Update app.py

--- Trabalho/app.py
+++ Trabalho/app.py
@@ -1,13 +1,15 @@
 from flask import *
-from bd import musica
-from bd import artista
+from bd import Musica, Artista, Estilo, Premio
+
 
 app = Flask(__name__)
 pl = Musica
 a = Artista
+e = Estilo
+p = Premio
 
 @app.route('/')
-def listar_musica():
+def listar_musicas():
     musicas = pl.listar_musicas()
     return render_template("musica.html", musicas=musicas)
 
@@ -59,6 +61,60 @@ def editar_artista(id_artista):
         return redirect('/artista')
     artista = a.detalha_artista(id_artista)
     return render_template('artista_form.html', artista=artista, title='Editar Artista')
+
+@app.route('/estilo')
+def listar_estilos():
+    artistas = a.listar_estilos()
+    return render_template("estilos.html", estilos=estilos)
+
+@app.route("/estilo/remover/<int:id_estilo>")
+def apagar_estilo(id_estilo):
+    e.remove_estilo(id_estilo)
+    return redirect('/estilo')
+
+@app.route("/estilo/novo", methods=['GET', 'POST'])
+def cadastro_estilo():
+    if request.method == 'POST':
+        ds = request.form.to_dict()
+        e.novo_estilo(dados.get('nome_estilo'))
+        return redirect('/estilo')
+    return render_template('estilo_form.html', estilo=None, title='Novo estilo')
+
+@app.route("/estilo/editar/<int:id_estilo>", methods=['GET', 'POST'])
+def editar_artista(id_estilo):
+    if request.method == 'POST':
+        ds = request.form.to_dict()
+        e.atualiza_estilo(id_estilo, ds.get('nome_estilo'))
+        return redirect('/estilo')
+    estilo = e.atualiza_artista(id_estilo)
+    return render_template('estilo_form.html', estilo=estilo, title='Editar Estilo')
+
+@app.route('/premio')
+def listar_premio():
+    premio = p.listar_premio()
+    return render_template("premio.html", premio=premio)
+
+@app.route("/artista/remover/<int:id_time>")
+def apagar_premio(id_premios):
+    p.remove_premio(id_premio)
+    return redirect('/premio')
+
+@app.route("/premio/novo", methods=['GET', 'POST'])
+def cadastro_premio():
+    if request.method == 'POST':
+        ds = request.form.to_dict()
+        p.novo_premio(dados.get('artista'), ds.get('premio'))
+        return redirect('/premio')
+    return render_template('premio_form.html', premio=None, title='Novo premio')
+
+@app.route("/premio/editar/<int:id_premio>", methods=['GET', 'POST'])
+def editar_premio(id_premio):
+    if request.method == 'POST':
+        ds = request.form.to_dict()
+        p.atualiza_premio(id_premio, ds.get('artista') ds.get('premio'))
+        return redirect('/premio')
+    artista = p.editar_premio(id_premio)
+    return render_template('premio_form.html', premio=premio, title='Editar Premio')
 
 
 
