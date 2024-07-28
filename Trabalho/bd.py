f286d18a4f4a6299d31256521fbc4a24e8ce5f17
Bielzindovasco <124696240+Bielzindovasco@users.noreply.github.com>
2024-07-27 22:13:21 -0300

    Update bd.py

--- Trabalho/bd.py
+++ Trabalho/bd.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-class Playlist:
+class Musica:
     def criar():
-        conn = sqlite3.connect('playlist.db')
+        conn = sqlite3.connect('musica.db')
         cursor = conn.cursor()
 
         cursor.execute("""
-            CREATE TABLE IF NOT EXISTS playlists(
+            CREATE TABLE IF NOT EXISTS musicas(
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 nome VARCHAR(255) NOT NULL,
                 tipo VARCHAR(255) NOT NULL,
@@ -16,41 +16,41 @@ class Playlist:
         conn.close()
 
 
-    def nova_playlist(nome, tipo, artista):
-        conn = sqlite3.connect('playlist.db')
+    def nova_musica(nome, tipo, artista):
+        conn = sqlite3.connect('musica.db')
         cursor = conn.cursor()
 
         cursor.execute("""
-            INSERT INTO playlists(nome, tipo, artista)
+            INSERT INTO musicas(nome, tipo, artista)
             VALUES(?, ?, ?);
         """, (nome, tipo, artista))
         conn.commit()
         conn.close()
 
 
-    def listar_playlist():
-        conn = sqlite3.connect('playlist.db')
+    def listar_musicas():
+        conn = sqlite3.connect('musica.db')
         cursor = conn.cursor()
-        values = cursor.execute("SELECT * FROM playlists")
+        values = cursor.execute("SELECT * FROM musicas")
         resultado = []
         for row in values:
             resultado.append({
                 'id': row[0],
                 'nome': row[1],
-                'tpo': row[2],
+                'tipo': row[2],
                 'artista': row[3],
             })
         conn.close()
         return resultado
 
 
-    def atualiza_playlist(id, nome, tipo, artista):
-        conn = sqlite3.connect('playlist.db')
+    def atualiza_musica(id, nome, tipo, artista):
+        conn = sqlite3.connect('musica.db')
         cursor = conn.cursor()
         cursor.execute(
             """
-            UPDATE playlists
-            SET nome=?, posicao=?, altura=?
+            UPDATE musicas
+            SET nome=?, tipo=?, artista=?
             WHERE id=?;
             """,
             (nome, tipo, artista, id)
@@ -59,12 +59,222 @@ class Playlist:
         conn.close()
 
 
-    def remove_playlist(id):
-        conn = sqlite3.connect('playlist.db')
+    def remove_musica(id):
+        conn = sqlite3.connect('musica.db')
         cursor = conn.cursor()
         cursor.execute(
             """
-            DELETE FROM playlists
+            DELETE FROM musicas
+            WHERE id=?;
+            """,
+            (id,)
+        )
+        conn.commit()
+        conn.close()
+
+class Artista:
+    def criar():
+        conn = sqlite3.connect('artista.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS artistas(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome VARCHAR(255) NOT NULL,
+                estilo_musica VARCHAR(255) NOT NULL,
+                sexo VARCHAR(10) NOT NULL);
+        """)
+
+        conn.close()
+
+
+    def novo_artista(nome, estilo_musica, sexo):
+        conn = sqlite3.connect('artista.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO artistas(nome, estilo_musica, sexo)
+            VALUES(?, ?, ?);
+        """, (nome, estilo_musica, sexo))
+        conn.commit()
+        conn.close()
+
+
+    def listar_artistas():
+        conn = sqlite3.connect('artista.db')
+        cursor = conn.cursor()
+        values = cursor.execute("SELECT * FROM artistas")
+        resultado = []
+        for row in values:
+            resultado.append({
+                'id': row[0],
+                'nome': row[1],
+                'estilo_musica': row[2],
+                'artista': row[3],
+            })
+        conn.close()
+        return resultado
+
+
+    def atualiza_artista(id, nome, estilo_musica, sexo):
+        conn = sqlite3.connect('artistas.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            UPDATE artistas
+            SET nome=?, estilo_musica=?, sexo=?
+            WHERE id=?;
+            """,
+            (nome, estilo_musica, sexo, id)
+        )
+        conn.commit()
+        conn.close()
+
+
+    def remove_artista(id):
+        conn = sqlite3.connect('artista.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            DELETE FROM artistas
+            WHERE id=?;
+            """,
+            (id,)
+        )
+        conn.commit()
+        conn.close()
+
+class Estilo:
+    def criar():
+        conn = sqlite3.connect('estilo.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS estilos(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome_estilo VARCHAR(255) NOT NULL,
+        """)
+
+        conn.close()
+
+
+    def novo_estilo(nome_estilo):
+        conn = sqlite3.connect('estilo.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO estilos(nome_estilos)
+            VALUES(?, ?, ?);
+        """, (nome_estilo))
+        conn.commit()
+        conn.close()
+
+
+    def listar_estilos():
+        conn = sqlite3.connect('estilo.db')
+        cursor = conn.cursor()
+        values = cursor.execute("SELECT * FROM estilos")
+        resultado = []
+        for row in values:
+            resultado.append({
+                'id': row[0],
+                'nome_estilos': row[1],
+            })
+        conn.close()
+        return resultado
+
+
+    def atualiza_estilo(id, nome_estilo):
+        conn = sqlite3.connect('estilo.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            UPDATE estilos
+            SET nome=?, posicao=?, altura=?
+            WHERE id=?;
+            """,
+            (nome_estilos, id)
+        )
+        conn.commit()
+        conn.close()
+
+
+    def remove_estilo(id):
+        conn = sqlite3.connect('estilo.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            DELETE FROM estilos
+            WHERE id=?;
+            """,
+            (id,)
+        )
+        conn.commit()
+        conn.close()
+
+class Premios:
+    def criar():
+        conn = sqlite3.connect('premio.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS premios(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                artista VARCHAR(255) NOT NULL,
+                premio VARCHAR(255) NOT NULL,
+        """)
+
+        conn.close()
+
+
+    def novo_premio(artista,premio):
+        conn = sqlite3.connect('premio.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO premios(artista, premio)
+            VALUES(?, ?, ?);
+        """, (artista, premio))
+        conn.commit()
+        conn.close()
+
+
+    def listar_premio():
+        conn = sqlite3.connect('premio.db')
+        cursor = conn.cursor()
+        values = cursor.execute("SELECT * FROM premio")
+        resultado = []
+        for row in values:
+            resultado.append({
+                'id': row[0],
+                'artista': row[1],
+                'premio': row[2],
+            })
+        conn.close()
+        return resultado
+
+
+    def atualiza_premio(id, artista, premio):
+        conn = sqlite3.connect('premio.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            UPDATE premios
+            SET artista=?, premio=?,
+            WHERE id=?;
+            """,
+            (artista, premio, id)
+        )
+        conn.commit()
+        conn.close()
+
+
+    def remove_premio(id):
+        conn = sqlite3.connect('premio.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            DELETE FROM premios
             WHERE id=?;
             """,
             (id,)
@@ -73,6 +283,8 @@ class Playlist:
         conn.close()
 
 
-
 if __name__=='__main__':
-    Playlist.criar()
+    Musica.criar()
+    Artista.criar()
+    Estilo.criar()
+    Premio.criar()
